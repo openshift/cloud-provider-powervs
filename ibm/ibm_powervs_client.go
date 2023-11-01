ec70de0acdf99a7d6444a04f7ad4ec9628d00784
Karthik K N <karthikkn1997@gmail.com>
2023-11-01 11:30:42 +0530

    UPSTREAM: <carry>: Support to use service instance name in config

--- ibm/ibm_powervs_client.go
+++ ibm/ibm_powervs_client.go
@@ -23,15 +23,28 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"net/url"
 	"strings"
 
 	"github.com/IBM-Cloud/power-go-client/clients/instance"
 	"github.com/IBM-Cloud/power-go-client/ibmpisession"
 	"github.com/IBM-Cloud/power-go-client/power/models"
 	"github.com/IBM/go-sdk-core/v5/core"
+	rc "github.com/IBM/platform-services-go-sdk/resourcecontrollerv2"
 
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/klog/v2"
+	"k8s.io/utils/pointer"
+)
+
+const (
+	//powerVSResourceID is Power VS power-iaas service id, can be retrieved using ibmcloud cli
+	// ibmcloud catalog service power-iaas
+	powerVSResourceID = "abd259f0-9990-11e8-acc8-b9f54a8f1661"
+
+	//powerVSResourcePlanID is Power VS power-iaas plan id, can be retrieved using ibmcloud cli
+	// ibmcloud catalog service power-iaas
+	powerVSResourcePlanID = "f165dd34-3a40-423b-9d95-e90a23f724dd"
 )
 
 // dhcpCacheStore is a cache store to hold the Power VS VM DHCP IP.
@@ -47,6 +60,7 @@ type Client interface {
 	GetInstanceByName(name string) (*models.PVMInstance, error)
 	GetDHCPServers() (models.DHCPServers, error)
 	GetDHCPServerByID(string) (*models.DHCPServerDetail, error)
+	GetCloudServiceInstanceByName(string) (*rc.ResourceInstance, error)
 }
 
 // ibmPowerVSClient makes call to IBM Cloud Power VS APIs
@@ -61,11 +75,15 @@ type powerVSClient struct {
 	cloudInstanceID string
 	instanceClient  *instance.IBMPIInstanceClient
 	dhcpClient      *instance.IBMPIDhcpClient
+	resourceClient  *rc.ResourceControllerV2
 }
 
 // newPowerVSSdkClient initializes a new sdk client and can be overridden by testing
-var newPowerVSSdkClient = func(provider Provider) (Client, error) {
-	credential, err := readCredential(provider)
+var newPowerVSSdkClient = func(provider *Provider) (Client, error) {
+	if provider.PowerVSCloudInstanceName == "" && provider.PowerVSCloudInstanceID == "" {
+		return nil, fmt.Errorf("both service instance id and name cannot be empty")
+	}
+	credential, err := readCredential(*provider)
 	if err != nil {
 		klog.Errorf("Failed to read the credentials, Error: %v", err)
 		return nil, err
@@ -90,10 +108,33 @@ var newPowerVSSdkClient = func(provider Provider) (Client, error) {
 		klog.Errorf("Failed to create new IBMPISession, Error: %v", err)
 		return nil, err
 	}
-
-	client := &powerVSClient{
-		cloudInstanceID: provider.PowerVSCloudInstanceID,
+	client := &powerVSClient{}
+	if provider.PowerVSCloudInstanceID == "" {
+		rcv2, err := rc.NewResourceControllerV2(&rc.ResourceControllerV2Options{
+			Authenticator: &core.IamAuthenticator{
+				ApiKey: credential,
+			},
+		})
+		if err != nil {
+			klog.Errorf("failed to create resource controller to fetch service instance id, error: %v", err)
+			return nil, err
+		}
+		if rcv2 == nil {
+			return nil, fmt.Errorf("unable to get resource controller")
+		}
+		client.resourceClient = rcv2
+		cloudInstance, err := client.GetCloudServiceInstanceByName(provider.PowerVSCloudInstanceName)
+		if err != nil {
+			klog.Errorf("failed to get serviceInstance with name %s, err: %v", err, provider.PowerVSCloudInstanceName)
+			return nil, err
+		}
+		if cloudInstance == nil {
+			return nil, fmt.Errorf("cannot fetch service instance with name %s, service instance is nil", provider.PowerVSCloudInstanceName)
+		}
+		// Update the cloud instance id to provider.PowerVSCloudInstanceID so that we can refer it while setting provider id
+		provider.PowerVSCloudInstanceID = *cloudInstance.GUID
 	}
+	client.cloudInstanceID = provider.PowerVSCloudInstanceID
 	ctx := context.Background()
 	client.instanceClient = instance.NewIBMPIInstanceClient(ctx, session, client.cloudInstanceID)
 	client.dhcpClient = instance.NewIBMPIDhcpClient(ctx, session, client.cloudInstanceID)
@@ -101,7 +142,7 @@ var newPowerVSSdkClient = func(provider Provider) (Client, error) {
 }
 
 // newPowerVSClient initializes a new validated powerVSClient
-func newPowerVSClient(provider Provider) (*ibmPowerVSClient, error) {
+func newPowerVSClient(provider *Provider) (*ibmPowerVSClient, error) {
 	// create Power VS sdk client
 	sdk, err := newPowerVSSdkClient(provider)
 	if err != nil {
@@ -110,14 +151,14 @@ func newPowerVSClient(provider Provider) (*ibmPowerVSClient, error) {
 	}
 
 	return &ibmPowerVSClient{
-		provider: provider,
+		provider: *provider,
 		sdk:      sdk,
 	}, nil
 }
 
 // isProviderPowerVS returns true when PowerVS specific parameters are set in Provider
 func isProviderPowerVS(provider Provider) bool {
-	if provider.PowerVSCloudInstanceID != "" && provider.PowerVSRegion != "" && provider.PowerVSZone != "" {
+	if (provider.PowerVSCloudInstanceID != "" || provider.PowerVSCloudInstanceName != "") && provider.PowerVSRegion != "" && provider.PowerVSZone != "" {
 		return true
 	}
 	return false
@@ -150,6 +191,58 @@ func (p *powerVSClient) GetDHCPServers() (models.DHCPServers, error) {
 
 func (p *powerVSClient) GetDHCPServerByID(id string) (*models.DHCPServerDetail, error) {
 	return p.dhcpClient.Get(id)
+}
+
+func (p *powerVSClient) GetCloudServiceInstanceByName(name string) (*rc.ResourceInstance, error) {
+	if name == "" {
+		return nil, fmt.Errorf("cannot fetch service instance as service instance name is empty")
+	}
+	var serviceInstancesList []rc.ResourceInstance
+	f := func(start string) (bool, string, error) {
+		listServiceInstanceOptions := &rc.ListResourceInstancesOptions{
+			Name:           &name,
+			ResourceID:     pointer.String(powerVSResourceID),
+			ResourcePlanID: pointer.String(powerVSResourcePlanID),
+		}
+		if start != "" {
+			listServiceInstanceOptions.Start = &start
+		}
+
+		serviceInstances, _, err := p.resourceClient.ListResourceInstances(listServiceInstanceOptions)
+		if err != nil {
+			return false, "", err
+		}
+		if serviceInstances != nil {
+			serviceInstancesList = append(serviceInstancesList, serviceInstances.Resources...)
+			nextURL, err := serviceInstances.GetNextStart()
+			if err != nil {
+				return false, "", err
+			}
+			if nextURL == nil {
+				return true, "", nil
+			}
+			return false, *nextURL, nil
+		}
+		return true, "", nil
+	}
+
+	if err := PagingHelper(f); err != nil {
+		return nil, fmt.Errorf("error listing service instances %v", err)
+	}
+	// log useful error message
+	switch len(serviceInstancesList) {
+	case 0:
+		errStr := fmt.Errorf("does exist any cloud service instance with name %s", name)
+		klog.Errorf(errStr.Error())
+		return nil, fmt.Errorf("does exist any cloud service instance with name %s", name)
+	case 1:
+		klog.Infof("serviceInstance %s found with ID: %v", name, serviceInstancesList[0].GUID)
+		return &serviceInstancesList[0], nil
+	default:
+		errStr := fmt.Errorf("there exist more than one service instance ID with with same name %s, Try setting serviceInstance.ID", name)
+		klog.Errorf(errStr.Error())
+		return nil, errStr
+	}
 }
 
 // populateNodeMetadata forms the node metadata from instance details
@@ -279,4 +372,49 @@ func getPowerVSNetwork(instance *models.PVMInstance) (*models.PVMInstanceNetwork
 		return nil, errStr
 	}
 	return instance.Networks[0], nil
+}
+
+// getStartToken parses the given url string and gets the 'start' query param.
+func getStartToken(nextURLS string) (string, error) {
+	nextURL, err := url.Parse(nextURLS)
+	if err != nil || nextURL == nil {
+		return "", fmt.Errorf("could not parse next url for getting next resources %w", err)
+	}
+
+	start := nextURL.Query().Get("start")
+	return start, nil
+}
+
+// PagingHelper while listing resources, can use this to get the start token for getting the next set of resources for processing
+// start token will get fetched from nextURL returned by f and passed to the func f.
+// f should take start as param and return three values isDone bool, nextURL string, e error.
+// isDone  - represents no need to iterate for getting next set of resources.
+// nextURL - if nextURL is present, will try to get the start token and pass it to f for next set of resource processing.
+// e       - if e is not nil, will break and return the error.
+func PagingHelper(f func(string) (bool, string, error)) error {
+	start := ""
+	var err error
+	for {
+		isDone, nextURL, e := f(start)
+
+		if e != nil {
+			err = e
+			break
+		}
+
+		if isDone {
+			break
+		}
+
+		// for paging over next set of resources getting the start token
+		if nextURL != "" {
+			start, err = getStartToken(nextURL)
+			if err != nil {
+				break
+			}
+		} else {
+			break
+		}
+	}
+	return err
 }
